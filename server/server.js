dbceb46e7973089d746894fc7ae47090421d4585
Mike Chung <m.chung1128@gmail.com>
2019-08-12 16:45:20 -0400

    Add get and post routes to server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const PORT = 3000;
+const db = require('../db/db.js');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser());
 app.use(express.static('client/dist'));
+
+app.get('/list', (req, res) => {
+  db.getAllItems()
+    .then(data => res.send(data))
+    .catch(err => console.log(`Error getting items: ${err}`))
+});
+
+app.post('/list', (req, res) => {
+  var data = req.body;
+  db.addItem(data)
+    .then(item => res.status(200).send(data).end())
+    .catch(err => console.log(`Error adding item: ${err}`))
+});
 
 app.listen(PORT, () => console.log(`Express server running on port ${PORT}`));
