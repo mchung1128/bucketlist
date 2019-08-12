50b93f866becd3d3930a44d52c669e3bd1f9886e
Mike Chung <m.chung1128@gmail.com>
2019-08-12 16:40:58 -0400

    Add get/post functionality and other components

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,81 @@
 import React from 'react';
+// import Input from './Input.jsx';
+import List from './List.jsx';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      category: null,
+      value: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleSubmit(e) {
+    var data = {
+      category: this.state.category,
+      item: this.state.value
+    }
+    fetch('/list', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(data)
+    })
+      .then(() => {
+        fetch('/list')
+          .then(res => res.json())
+          .then(list => this.setState({
+            items: list
+          }))
+          .catch(err => console.log(`Error fetching new list: ${err}`))
+      })
+      .catch(err => console.log(`Error posting new item: ${err}`))
+      e.preventDefault();
+  }
+
+  componentDidMount() {
+    fetch('/list')
+      .then(res => res.json())
+      .then(data => this.setState({
+        items: data
+      }))
+      .catch(err => console.log(`Error fetching list!`))
+  }
+
   render() {
     return (
-      <div>Bucket List Test</div>
+      <div>
+        <h3>Get Out and Live</h3>
+        <form>
+          <label>Category:
+            <select name="category" onChange={this.handleChange}>
+              <option>Choose one</option>
+              <option>Places I want to go</option>
+              <option>Things I want to eat/drink</option>
+              <option>Skills I want to learn</option>
+              <option>Things I want to do</option>
+              <option>Books I want to read</option>
+              <option>TV Shows/Movies I want to watch</option>
+            </select>
+          </label>
+          <label>
+            <input type="text" name="value" onChange={this.handleChange}></input>
+          </label>
+          <input type="submit" onClick={this.handleSubmit}/>
+        </form>
+        <List items={this.state.items}/>
+      </div>
     )
   }
 }
