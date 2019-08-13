f914f098966c8eeeb784289ab5e1b1a51e43d878
Mike Chung <m.chung1128@gmail.com>
2019-08-13 18:59:17 -0400

    Add styled components and material ui

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import styled from 'styled-components';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
+const Form = styled.form`
+  display: flex;
+  align-items: center;
+  margin: auto;
+  background-color: #e9e4e6;
+`
+
+const Label = styled.span`
+  font-family: 'Ubuntu', sans-serif;
+  font-size: 1.5em;
+  margin-left: 0.25em;
+`
+
+const Select = styled.select`
+  margin-left: 1.5em;
+`
 
 const Input = ({ handleChange, handleSubmit }) => {
   return (
-    <form>
-      <label>Category:
-        <select name="category" onChange={handleChange}>
+    <Form>
+      <Label>Category:
+        <Select name="category" onChange={handleChange}>
           <option>Choose one</option>
           <option>Places I want to go</option>
           <option>Things I want to eat/drink</option>
@@ -12,13 +34,22 @@ const Input = ({ handleChange, handleSubmit }) => {
           <option>Things I want to do</option>
           <option>Books I want to read</option>
           <option>TV Shows/Movies I want to watch</option>
-        </select>
-      </label>
-      <label>
-        <input type="text" name="value" onChange={handleChange}></input>
-      </label>
-      <input type="submit" onClick={handleSubmit}/>
-    </form>
+        </Select>
+      </Label>
+      <Label>
+      <TextField
+        id="standard-bare"
+        placeholder="Fill me in"
+        margin="normal"
+        inputProps={{ 'aria-label': 'bare' }}
+        onChange={handleChange}
+        name="value"
+      />
+      </Label>
+      <Button onClick={handleSubmit} variant="contained" color="primary">
+        Submit
+      </Button>
+    </Form>
   )
 }
 
